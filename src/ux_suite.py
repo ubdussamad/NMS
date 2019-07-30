690f12572b459ad39f2877ab9d55c209a265e2a6
ubdussamad <ubdussamad@gmail.com>
2019-07-30 18:10:13 +0530

    Added New Stuff ,Fixed some bugs

--- src/ux_suite.py
+++ src/ux_suite.py
@@ -2,12 +2,37 @@
 # Ref: Jul 30th , 2019
 # Author: ubdussamad <[name][at][google's email]>
 from api_applet import yahoo_api
-import math,time
+import math,time , json
+import pandas as pd
+
+STATION = yahoo_api('kf.pyc')
+
 class suite (object):
 	def integrate_functionality(self):
-		self.pushButton_2.clicked.connect(self.dummy)
+		self.pushButton_5.clicked.connect(self.refresh)
+		self.lineEdit.setPlaceholderText("Karnataka")
 	def dummy(self):
 		print("I am a dummy!")
+	def refresh(self):
+		location = self.lineEdit.text()
+		data = STATION.query_(location if location else "Karnataka")
+
+		#df = pd.DataFrame(data=data)
+		try:
+			temp = data['current_observation']['condition']['temperature']
+			wind_speed = data['current_observation']['wind']['speed']
+			wind_dirn = data['current_observation']['wind']['direction']
+		except:
+			self.statusbar.showMessage("Invalid Location Input!",4000)
+			temp = "-- "
+			wind_speed = "<i>N.A </i>"
+			wind_dirn = "<i>N.A </i>"
+
+		self.label_3.setText("<b>Wind</b>: %sKm/hr | <b>Dir</b>: %s°N"%(str(wind_speed),
+			str(wind_dirn)))
+		self.lcdNumber.display("%sC"%str(temp))
+		self.textBrowser.setHtml(json.dumps(data['location'])+"<br/><br/><br/><br/>"+json.dumps(data['current_observation']))
+
 
 if __name__ == "__main__":
 	print("Hi this is a text text to see if all imports pass!")
